c86f77ab87f57b48300d41d2056f395593098833
Alexey Pakhomov <alexeyt101@mail.ru>
2021-04-14 22:50:06 +0300

    First commit

---
+++ config.py
@@ -0,0 +1,9 @@
+BASE_CURENT_WEATHER_URL = 'https://tgftp.nws.noaa.gov/data/observations/metar/stations/<airport_ICAO_code>.TXT'
+BASE_WEATHER_FORECAST_URL = 'https://tgftp.nws.noaa.gov/data/forecasts/taf/stations/<airport_ICAO_code>.TXT'
+
+airports_icao_codes = {
+    'Шереметьево': 'UUEE',
+    'Домодедово': 'UUDD',
+    'Внуково': 'UUWW',
+    'Пулково': 'ULLI', 
+}
